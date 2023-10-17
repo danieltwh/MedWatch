b22ff216cb4ab0cfc4bd3f0ebd298f1e685adb2b
danieltwh <71161486+danieltwh@users.noreply.github.com>
2023-10-18 01:26:44 +0800

    Add heart rate endpoint to serve hourly data

--- backend/app/routes/heartrate.py
+++ backend/app/routes/heartrate.py
@@ -83,3 +83,68 @@ async def get_heartrate(patientId: int, user: Annotated[User, Depends(authentica
     }
 
     return Response(content=json.dumps(resp_body), media_type="application/json")
+
+
+@router.get(
+    "/{patientId}/hour",
+    response_model=List[str],
+    tags=["Health Data"],
+    responses={
+        200: {
+            "content": {
+                "application/json": {
+                    "example": {
+                        "data": [
+                            {
+                                "id": 1,
+                                "time": "2016-04-12T07:21:00",
+                                "value": 97.0
+                            },
+                            {
+                                "id": 1,
+                                "time": "2016-04-12T07:21:05",
+                                "value": 102.0
+                            }
+                        ]
+                    }
+                }
+            },
+            "description": """Returns heartrate details for the patient""",
+        },
+    },
+)
+async def get_heartrate_hour(patientId: int, user: Annotated[User, Depends(authenticate_user)]) -> List[HeartRateResponse]:
+    curr_now = datetime.datetime.utcnow() + datetime.timedelta(minutes=480)
+
+    # curr_now = curr_now.replace(year=2023, month=10, day=15, hour = 12, minute=0, second=0)
+
+    time_limit = curr_now - datetime.timedelta(minutes=60)
+    print(time_limit)
+    results = await HeartRate.find(
+            HeartRate.patientId == patientId,
+            HeartRate.time >= time_limit.isoformat()
+            ).to_list()
+
+
+    data = []
+    for result in results:
+        data.append({
+            "id": result.patientId,
+            "time": result.time,
+            "value": result.value
+        })
+
+    for d in data:
+        # d['time'] = datetime.datetime.strptime(d['time'], "%d/%m/%Y %I:%M:%S %p")
+        d['time'] = datetime.datetime.fromisoformat(d['time'])
+    
+    data.sort(key=lambda x: (x['time'], x["id"]))
+
+    for d in data:
+        d['time'] = d['time'].isoformat()
+    
+    resp_body = {
+        "data": data
+    }
+
+    return Response(content=json.dumps(resp_body), media_type="application/json")
--- services/fitbit_stream/heartrate_fetcher.py
+++ services/fitbit_stream/heartrate_fetcher.py
@@ -93,10 +93,14 @@ async def fetch_heartrate():
     
     # end = datetime.datetime.utcnow() + datetime.timedelta(minutes=480) - datetime.timedelta(days=2)
 
-    curr_now = datetime.datetime.utcnow()
+    curr_now = datetime.datetime.utcnow() + datetime.timedelta(minutes=480)
 
+    # end = datetime.datetime(year = 2023, month = 10, day = 15, 
+    #                         hour = curr_now.hour, minute=curr_now.minute, second=curr_now.second)
+    
     end = datetime.datetime(year = 2023, month = 10, day = 15, 
-                            hour = curr_now.hour, minute=curr_now.minute, second=curr_now.second)
+                        hour = 12, minute=0, second=0)
+
 
     start = end - datetime.timedelta(minutes=1)
 
@@ -131,11 +135,12 @@ async def fetch_heartrate():
 async def main():
     await init_mongodb()
     while True:
+        curr_now = datetime.datetime.utcnow() + datetime.timedelta(minutes=480)
         try:
             await fetch_heartrate()
-            print("Completed")
+            print(f"{curr_now.isoformat()} - Completed")
         except Exception as err:
-            print("Failed to fetch all heartrate")
+            print(f"{curr_now.isoformat()} - Failed to fetch all heartrate")
             print(err)
         finally:
             time.sleep(10)

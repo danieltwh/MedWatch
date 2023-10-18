27147245a558137b7e9f03f95fce11c297194b6f
danieltwh <71161486+danieltwh@users.noreply.github.com>
2023-10-18 18:11:39 +0800

    Add python script to poll available heart rate data

--- services/fitbit_stream/heartrate_fetcher.py
+++ services/fitbit_stream/heartrate_fetcher.py
@@ -89,18 +89,17 @@ async def fetch_user_heartrate(patient_id, patient_access_token, heart_rate_url,
 
 
 async def fetch_heartrate():
-    
-    
-    # end = datetime.datetime.utcnow() + datetime.timedelta(minutes=480) - datetime.timedelta(days=2)
-
-    curr_now = datetime.datetime.utcnow() + datetime.timedelta(minutes=480)
+    end = datetime.datetime.utcnow() + datetime.timedelta(minutes=480)
 
     # end = datetime.datetime(year = 2023, month = 10, day = 15, 
     #                         hour = curr_now.hour, minute=curr_now.minute, second=curr_now.second)
     
-    end = datetime.datetime(year = 2023, month = 10, day = 15, 
-                        hour = 12, minute=0, second=0)
+    # end = datetime.datetime(year = 2023, month = 10, day = 15, 
+    #                     hour = 12, minute=0, second=0)
+    
+    # end = datetime.datetime.utcnow() + datetime.timedelta(minutes=480) - datetime.timedelta(days=2)
 
+    # end = datetime.datetime.utcnow() + datetime.timedelta(minutes=480)
 
     start = end - datetime.timedelta(minutes=1)
 
